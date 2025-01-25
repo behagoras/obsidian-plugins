e4d2b4df7fe6d0e5e2bf7827fed7396049d99c5b
David Behar <davidbehar@Davids-MacBook-Pro-2.local>
2025-01-25 17:56:06 -0600

    Fix double Enter bug + Add custom styles

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ export default class CustomEnterPlugin extends Plugin {
         evt.preventDefault();
         if (activeEditor && activeEditor.editor) {
           // Insert two line breaks
-          activeEditor.editor.replaceSelection("\n\n");
+          activeEditor.editor.replaceSelection("\n");
         }
       }
       // Check if the Enter key is pressed with Shift
@@ -24,14 +24,41 @@ export default class CustomEnterPlugin extends Plugin {
         evt.preventDefault();
         if (activeEditor && activeEditor.editor) {
           // Insert one line break
-          activeEditor.editor.replaceSelection("\n");
+          // activeEditor.editor.replaceSelection("\n");
         }
       }
     });
+
+    this.addCustomStyles();
+
   }
+
+  addCustomStyles() {
+    const style = document.createElement("style");
+    style.setAttribute("id", "custom-enter-plugin-style");
+    style.innerHTML = `
+      /* Apply styles only in reading and live preview modes */
+      .markdown-preview-view p {
+        border: 1px solid blue; /* Border for paragraphs */
+        margin-bottom: 0.8em; /* Spacing for orphaned line */
+      }
+
+      .markdown-preview-view p + p {
+        border: 1px solid red; /* Border for joined lines */
+        margin-top: 0.5em; /* Spacing for joined lines */
+        margin-bottom: 0.5em; /* Spacing for joined lines */
+      }
+    `;
+    document.head.appendChild(style);
+  }
+
 
   // This method is called when the plugin is unloaded by Obsidian
   onunload(): void {
     console.log("Custom Enter Behavior Plugin unloaded");
+    const styleElement = document.getElementById("custom-enter-plugin-style");
+    if (styleElement) {
+      styleElement.remove();
+    }
   }
 }
