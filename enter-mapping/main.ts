f04ecffd4bcc2dd5d7d9d2ef6488ef3faac3e429
David Behar <davidbehar@Davids-MacBook-Pro-2.local>
2025-01-26 00:58:04 -0600

    calculate double enters

--- enter-mapping/main.ts
+++ enter-mapping/main.ts
@@ -4,16 +4,19 @@ export default class CustomEnterPlugin extends Plugin {
   async onload(): Promise<void> {
     console.log("Enter Mappings Plugin loaded");
 
-    // Listen for keyboard events in the active editor
     this.registerDomEvent(document, "keydown", (evt: KeyboardEvent) => {
       const activeEditor = this.app.workspace.activeEditor;
       const isEditMode = !this.isSourceMode();
+
+      const doubleEnter = this.isDoubleEnter();
+
       if (
         evt.key === "Enter" &&
         !evt.shiftKey &&
         activeEditor &&
         isEditMode &&
-        activeEditor.editor
+        activeEditor.editor &&
+        doubleEnter
       ) {
         console.log('Enter hit')
         activeEditor.editor.replaceSelection("\n");
@@ -21,9 +24,39 @@ export default class CustomEnterPlugin extends Plugin {
     });
   }
 
-  isSourceMode() {
+  private isSourceMode() {
     const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
     return activeView && !!activeView?.getState().source;
+  }
+
+
+  /**
+   * Checks if the current cursor position in the editor indicates a double Enter key press.
+   * 
+   * This function determines if the current line is empty and the previous line is not empty,
+   * which would indicate that the user has pressed Enter twice consecutively.
+   * 
+   * @returns {boolean} - Returns `true` if the current line is empty and the previous line is not empty,
+   *                      indicating a double Enter key press. Returns `false` otherwise.
+   */
+  private isDoubleEnter(): boolean {
+    const activeEditor = this.app.workspace.activeEditor;
+    if (!activeEditor || !activeEditor.editor) return false;
+
+    const editor = activeEditor.editor;
+    const cursor = editor.getCursor();
+    const currentLine = editor.getLine(cursor.line - 1);
+    const previousLine = cursor.line > 0 ? editor.getLine(cursor.line - 2) : null;
+
+    const bulletPointRegex = /^(\s*[-*+]\s)/;
+    const numberedListRegex = /^(\s*\d+\.\s)/;
+
+    if (bulletPointRegex.test(currentLine) || numberedListRegex.test(currentLine)) {
+      return false; // is a bullet point or numbered list
+    }
+    if (currentLine !== "" || previousLine === "") return true;
+
+    return false;
   }
 
   onunload(): void {
